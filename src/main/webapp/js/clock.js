cb237339313e55692bfbecc1ac40223a775eefee
Alohhhis <D_Alyona35@mail.ru>
2023-12-14 02:40:35 +0300

    .

--- src/main/webapp/js/clock.js
+++ src/main/webapp/js/clock.js
@@ -1,22 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-    updateClock();
+window.onload = function(){
+    function getTime() {
+        let now = new Date();
+        let clock = document.getElementById("clock");
+        clock.innerHTML = now.toLocaleTimeString();
+    }
+    getTime();
+    window.setInterval(function (){
+        getTime();
 
-    setInterval(updateClock, 6000);
-});
-
-function updateClock() {
-    let currentTime = new Date();
-    const offset = 3; // (UTC+3)
-    let moscowTime = new Date(currentTime.getTime() + offset * 60 * 60 * 1000);
-
-    let hours = moscowTime.getHours();
-    let minutes = moscowTime.getMinutes();
-    let seconds = moscowTime.getSeconds();
-
-    let clockDiv = document.getElementById("clock");
-    clockDiv.innerHTML = addZeroBefore(hours) + ":" + addZeroBefore(minutes) + ":" + addZeroBefore(seconds);
-}
-
-function addZeroBefore(number) {
-    return (number < 10 ? "0" : "") + number;
-}
+    }, 6000);
+};
